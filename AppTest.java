7f8887cadab727c92979d977c86b3b091ab54021
Anisiya99 <ab.joshy99@gmail.com>
2026-04-30 13:42:27 +0530

    Create AppTest.java

---
+++ AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    void testGreet() {
+        assertEquals("Hello, DevOps!", App.greet("DevOps"));
+    }
+}
